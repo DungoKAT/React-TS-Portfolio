c289e802107cc6ed267c5cf89ed19d378e3b0a3a
Punnaphat Phiamtanapuwanon <punnaphat.zaza@gmail.com>
2023-11-08 16:07:11 +0700

    Create Footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Copyright &copy; 2023 Punnaphat Phiamtanapuwanon. All right reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
